4a3ede94257b3ecbfa5dcdd72de265025e04c581
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-12-08 13:03:28 -0300

    Agregado slider

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ document.addEventListener('scroll', function(e) {
         botonHidden.classList.remove('boton--oculto');
     }
 });
+
+const slider = tns({
+    container: '.planes-wrapper',
+    items: 1,
+    center: true,
+    mouseDrag: true,
+    swipeAngle: false,
+    controlsText: [
+        "<i class='fa fa-caret-left'></i>",
+        "<i class='fa fa-caret-right'></i>"
+    ],
+    responsive: {
+        700: {
+            items: 2,
+            center: false,
+            autoWidth: true
+        },
+        900: {
+            items: 3,
+            center: false,
+            autoWidth: true
+        }
+    },
+    preventScrollOnTouch: 'auto'
+});
